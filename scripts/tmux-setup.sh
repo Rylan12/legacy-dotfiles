98d595d25e90e1d01512308609f5b7d6812945a4
Rylan Polster <rslpolster@gmail.com>
2020-05-08 18:31:26 -0400

    Add scripts directory and tmux setup script

---
+++ scripts/tmux-setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+BASE_SESSION="terminal"
+
+CURRENT_SESSIONS=$(/usr/local/bin/tmux list-sessions 2> /dev/null | grep "^$BASE_SESSION\(-\d\)\?: .*\$" | awk '{print $1}' | sed 's/.$//')
+
+# Determine name of session (terminal or terminal-n)
+if [ -n "$CURRENT_SESSIONS" ]; then
+    MAX=$(echo "$CURRENT_SESSIONS" | sed "s/^$BASE_SESSION\$/$BASE_SESSION-0/" | grep -o '.$' | sort -nr | head -n1)
+    SESSION_NAME="$BASE_SESSION-$((MAX + 1))"
+else
+    SESSION_NAME="$BASE_SESSION"
+fi
+
+# Start session (with message if there are detached sessions)
+if /usr/local/bin/tmux list-sessions 2> /dev/null | grep -vq '^.* (attached)$'; then
+    /usr/local/bin/tmux new -s "$SESSION_NAME" \; display-message "WARNING: detached sessions" > /dev/null 2>&1
+else
+    /usr/local/bin/tmux new -s "$SESSION_NAME" > /dev/null 2>&1
+fi
+
+# After exiting, kill session if it is not attached
+if ! /usr/local/bin/tmux list-sessions | grep -q "^$SESSION_NAME: .* (attached)\$"; then
+    /usr/local/bin/tmux kill-session -t "$SESSION_NAME" > /dev/null 2>&1
+    echo "[kill-session $SESSION_NAME]"
+else
+    echo "[detach from $SESSION_NAME]"
+fi
