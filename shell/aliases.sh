b07abdcdf5138e26eae04bef963d180d9668e8d1
Rylan Polster <rslpolster@gmail.com>
2020-11-14 03:40:12 -0500

    Add rpv and rpvw aliases for `gh repo view`

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -30,6 +30,8 @@ alias gh='PAGER= gh' # remove pager
 alias prc='gh pr checkout'
 alias prv='gh pr view'
 alias prvw='gh pr view -w' # open PR in web browser
+alias rpv='gh repo view'
+alias rpvw='gh repo view -w' # open repo in web browser
 
 # Pipe to clipboard
 alias pb='pbcopy'
