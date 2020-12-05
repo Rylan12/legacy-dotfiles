bc6d11200f4af8f3f413b9b208a558b636304867
Rylan Polster <rslpolster@gmail.com>
2020-12-05 15:48:33 -0500

    aliases: remove up functino

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -126,29 +126,6 @@ mbk() {
     fi
 }
 
-# Go up [n] directories
-up()
-{
-    local cdir="$(pwd)"
-    if [[ "${1}" == "" ]]; then
-        cdir="$(dirname "${cdir}")"
-    elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
-        echo "Error: argument must be a number"
-    elif ! [[ "${1}" -gt "0" ]]; then
-        echo "Error: argument must be positive"
-    else
-        for ((i=0; i<${1}; i++)); do
-            local ncdir="$(dirname "${cdir}")"
-            if [[ "${cdir}" == "${ncdir}" ]]; then
-                break
-            else
-                cdir="${ncdir}"
-            fi
-        done
-    fi
-    cd "${cdir}"
-}
-
 # Execute a command in a specific directory
 xin() {
     (
