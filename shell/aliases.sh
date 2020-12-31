7dcc20d3254f1e0d4977d8912f3eb8d87bac8856
Rylan Polster <rslpolster@gmail.com>
2020-12-31 18:44:47 -0500

    aliases: update venv command

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -76,7 +76,7 @@ extract() {
 # Create and source virtual environment
 venv() {
     [[ -d venv ]] && echo "venv directory already exists" 1>&2 && return 1
-    virtualenv venv && source venv/bin/activate
+    python3 -m venv venv && source venv/bin/activate
 }
 source-venv() {
     [[ ! -f venv/bin/activate ]] && echo "venv/bin/activate doesn't exist" 1>&2 && return 1
