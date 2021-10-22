30950b0c5fe2d44eba7da2abbcfc03f222dbd0e5
Rylan Polster <rslpolster@gmail.com>
2021-10-22 12:34:58 -0400

    Re-add `src` to reload config

--- zsh/config_after.zsh
+++ zsh/config_after.zsh
@@ -1,6 +1,9 @@
 # iTerm2 Shell Integration
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
+# reload configuration
+alias src='omz reload'
+
 # tmux iTerm2 Integration
 # ZSH_TMUX_ITERM2=true
 
