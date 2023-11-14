786bf2b4ffcdb7c9e269077d924d8ca54f21a856
Rylan Polster <rslpolster@gmail.com>
2023-11-14 16:44:53 -0500

    Add `ee428` alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -39,7 +39,8 @@ work() {
 
     open "$WORKSPACE_FILE"
 }
-alias cs480='cd "$HOME/Documents/Yale/CPSC 480/Workspace.nosync" && source venv/bin/activate'
+alias cs480='cd "$HOME/Documents/Yale/CPSC 480/Workspace.nosync/" && source venv/bin/activate'
+alias ee428='cd "$HOME/Development/EENG-428/"'
 
 # reload current directory
 alias reload='[[ ! -d "$(pwd)" ]] && echo "$(pwd) no longer exists" && return 1 || cd "$(pwd)"'
